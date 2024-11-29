9cb86cc385c845170baa4d04172623f6b0fb8406
Danil Kargin <danilkargin16@gmail.com>
2024-11-29 16:44:04 +0400

    Initial commit

---
+++ src/main/java/com/example/zvon73/Zvon73Application.java
@@ -0,0 +1,13 @@
+package com.example.zvon73;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zvon73Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zvon73Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zvon73/Zvon73ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zvon73;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zvon73ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

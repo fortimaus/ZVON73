3a67a511eeed6e3c4ed2cb205b8d2b29d853c355
Danil Kargin <danilkargin16@gmail.com>
2024-12-14 22:40:36 +0400

    Finished order controller

--- src/main/java/com/example/zvon73/DTO/OrderDto.java
+++ src/main/java/com/example/zvon73/DTO/OrderDto.java
@@ -22,24 +22,24 @@ public class OrderDto {
     private String id;
 
 
-    private UUID temple_start ;
+    private String temple_start ;
 
     private String temple_start_title;
 
 
-    private UUID temple_end ;
+    private String temple_end ;
 
     private String temple_end_title;
 
-    private UUID bellTower_start ;
+    private String bellTower_start ;
 
     private String bellTower_start_title;
 
-    private UUID bellTower_end = null;
+    private String bellTower_end = null;
 
     private String bellTower_end_title = "";
 
-    private UUID bell ;
+    private String bell ;
 
     private String bell_title;
 
@@ -50,18 +50,18 @@ public class OrderDto {
 
     public OrderDto(Order order){
         this.id = order.getId().toString();
-        this.temple_start = order.getTemple_start().getId();
+        this.temple_start = order.getTemple_start().getId().toString();
         this.temple_start_title = order.getTemple_start().getTitle();
-        this.temple_end = order.getTemple_end().getId();
+        this.temple_end = order.getTemple_end().getId().toString();
         this.temple_end_title = order.getTemple_end().getTitle();
-        this.bellTower_start = order.getBellTower_start().getId();
+        this.bellTower_start = order.getBellTower_start().getId().toString();
         this.bellTower_start_title = order.getBellTower_start().getTitle();
         if(order.getTemple_end()!=null) {
-            this.bellTower_end = order.getBellTower_end().getId();
+            this.bellTower_end = order.getBellTower_end().getId().toString();
             this.bellTower_end_title = order.getBellTower_end().getTitle();
         }
 
-        this.bell = order.getBell().getId();
+        this.bell = order.getBell().getId().toString();
         this.bell_title = order.getBell().getTitle();
         this.date = order.getDate().toString();
         this.status = order.getStatus().toString();
--- src/main/java/com/example/zvon73/service/OrdersService.java
+++ src/main/java/com/example/zvon73/service/OrdersService.java
@@ -1,6 +1,7 @@
 package com.example.zvon73.service;
 
 import com.example.zvon73.DTO.OrderDto;
+import com.example.zvon73.controller.domain.OrderRequest;
 import com.example.zvon73.entity.*;
 import com.example.zvon73.entity.Enums.BellStatus;
 import com.example.zvon73.entity.Enums.OrderStatus;
@@ -13,6 +14,7 @@ import org.webjars.NotFoundException;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,11 +29,11 @@ public class OrdersService {
     public Order create(OrderDto order)
     {
         User curUser = userService.getCurrentUser();
-        Temple templeStart = templeService.findById(order.getTemple_start());
+        Temple templeStart = templeService.findById(UUID.fromString(order.getTemple_start()));
         if(curUser.getId() == templeStart.getUser().getId()) {
-            Temple templeEnd = templeService.findById(order.getTemple_end());
-            BellTower bellTowerStart = bellTowerService.findById(order.getBellTower_start());
-            Bell bell = bellService.findById(order.getBell());
+            Temple templeEnd = templeService.findById(UUID.fromString(order.getTemple_end()));
+            BellTower bellTowerStart = bellTowerService.findById(UUID.fromString(order.getBellTower_start()));
+            Bell bell = bellService.findById(UUID.fromString(order.getBell()));
             Order newOrder = Order.builder()
                     .temple_start(templeStart)
                     .temple_end(templeEnd)
@@ -53,29 +55,29 @@ public class OrdersService {
     }
 
     @Transactional(readOnly = true)
-    public List<Order> findOldByOperatorId(UUID id){
-        return orderRepository.findOldByOperatorId(id);
+    public List<OrderDto> findOldByOperatorId(UUID id){
+        return orderRepository.findOldByOperatorId(id).stream().map(OrderDto::new).collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
-    public List<Order> findNewByOperatorId(UUID id){
-        return orderRepository.findNewOrdersByOperatorId(id);
+    public List<OrderDto> findNewByOperatorId(UUID id){
+        return orderRepository.findNewOrdersByOperatorId(id).stream().map(OrderDto::new).collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
-    public List<Order> findForModeration(){
-        return orderRepository.findForModeration();
+    public List<OrderDto> findForModeration(){
+        return orderRepository.findForModeration().stream().map(OrderDto::new).collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
-    public List<Order> findInPathForModerator(){
-        return orderRepository.findInPath();
+    public List<OrderDto> findInPathForModerator(){
+        return orderRepository.findInPath().stream().map(OrderDto::new).collect(Collectors.toList());
     }
 
     @Transactional
-    public Order updateStatusOnModeration(UUID id){
+    public Order updateStatusOnModeration(OrderRequest request){
         User curUser = userService.getCurrentUser();
-        Order currentOrder = findById(id);
+        Order currentOrder = findById(UUID.fromString(request.getOrder()));
         if (curUser.getId() == currentOrder.getTemple_end().getUser().getId()) {
             currentOrder.setStatus(OrderStatus.Waiting_moderator);
             return orderRepository.save(currentOrder);
@@ -85,24 +87,24 @@ public class OrdersService {
             throw new RuntimeException("Не имеешь права, хулиган");
     }
 
-    public Order updateBellTowerEnd(UUID idOrder ,UUID idTower){
-        Order currentOrder = findById(idOrder);
-        BellTower bellTower = bellTowerService.findById(idTower);
+    public Order updateBellTowerEnd(OrderRequest request){
+        Order currentOrder = findById(UUID.fromString(request.getOrder()));
+        BellTower bellTower = bellTowerService.findById(UUID.fromString(request.getBellTower()));
         currentOrder.setBellTower_end(bellTower);
         return orderRepository.save(currentOrder);
     }
 
     @Transactional
-    public Order updateStatusOnPath(UUID id){
-        Order currentOrder = findById(id);
+    public Order updateStatusOnPath(OrderRequest request){
+        Order currentOrder = findById(UUID.fromString(request.getOrder()));
         currentOrder.setStatus(OrderStatus.In_path);
         bellService.updateStatusInPath(currentOrder.getBell().getId());
         return orderRepository.save(currentOrder);
     }
 
     @Transactional
-    public Order updateStatusOnFinished(UUID id){
-        Order currentOrder = findById(id);
+    public Order updateStatusOnFinished(OrderRequest request){
+        Order currentOrder = findById(UUID.fromString(request.getOrder()));
         currentOrder.setStatus(OrderStatus.Finished);
         currentOrder.setDate(new Date());
         bellService.updateStatusAccepted(currentOrder.getBell().getId());
@@ -110,8 +112,8 @@ public class OrdersService {
     }
 
     @Transactional
-    public Order updateStatusOnCancelled(UUID id){
-        Order currentOrder = findById(id);
+    public Order updateStatusOnCancelled(OrderRequest request){
+        Order currentOrder = findById(UUID.fromString(request.getOrder()));
         currentOrder.setStatus(OrderStatus.Cancelled);
         bellService.updateStatusAccepted(currentOrder.getBell().getId());
         return orderRepository.save(currentOrder);
